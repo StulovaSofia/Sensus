15ea7c12956eca066a68c730250a0a108e6ec19b
Sonya2114 <stulova.2004@mail.ru>
2022-07-27 15:54:10 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+	// $('.menu-btn').on('click', function () {
+	// 	$('.menu__list').toggleClass('active');
+	// });
+	$('.slider').slick({
+		arrows: true,
+		slidesToShow: 3,
+		infinite: true,
+		// responsive: [
+		// 	{
+		// 		breakpoint: 841,
+		// 		settings: {
+		// 			slidesToShow: 2,
+		// 		}
+		// 	},
+		// 	{
+		// 		breakpoint: 601,
+		// 		settings: {
+		// 			slidesToShow: 1,
+		// 		}
+		// 	},
+		// ]
+	});
+
+});
