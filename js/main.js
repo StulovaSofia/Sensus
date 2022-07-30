4c4629d0e5db312e633a88b0da8254a50e018ff4
Sonya2114 <stulova.2004@mail.ru>
2022-07-30 18:57:48 +0700

    2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,31 @@ $(function () {
 		})
 	}
 });
+
+// всем блокам прописываем style="display: none"
+// кнопкам добавляем функции в onclick="nextBlock()"/"previousBlock()"
+const blockName = "block"; // то что идет до цифры в id блока
+const countBlocks = 13; // кол-во блоков, всего
+
+let currentBlock = 1; // текущий блок, не трогаем
+
+function nextBlock() {
+	document.getElementById(blockName + currentBlock).style.display = "none";
+	currentBlock = currentBlock < countBlocks ? currentBlock + 1 : currentBlock;
+	document.getElementById(blockName + currentBlock).style.display = "block";
+}
+
+function previousBlock() {
+	document.getElementById(blockName + currentBlock).style.display = "none";
+	currentBlock = currentBlock > 1 ? currentBlock - 1 : currentBlock;
+	document.getElementById(blockName + currentBlock).style.display = "block";
+}
+
+let img = 0;
+
+function changeBlock(numBlock) {
+	if (img !== 0) document.getElementById("img" + img).style.display = "none";
+	else document.getElementById("button1").disabled = false;
+	img = numBlock;
+	document.getElementById("img" + img).style.display = "block";
+}
